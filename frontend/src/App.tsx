66b89ad6c865122642f7ee5e9186712696c7117b
Gabrielle Lima <gabrielle.clima23@gmail.com>
2022-01-12 01:23:22 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "components/navbar";
-
+import Listing from "pages/Listing";
+import Form from "pages/Form";
 function App() {
-  return <Navbar />;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
