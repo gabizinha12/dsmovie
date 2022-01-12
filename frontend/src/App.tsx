65340b8979c817615402558be8f8ac3ebcba77cf
Gabrielle Lima <gabrielle.clima23@gmail.com>
2022-01-12 01:11:22 -0300

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,7 @@
-import {ReactComponent as GithubIcon} from './assets/img/github.svg';
+import Navbar from "components/navbar";
 
 function App() {
-  return (
-    <header>
-    <nav className="container">
-        <div className="dsmovie-nav-content">
-            <h1>DSMovie</h1>
-            <a href="https://github.com/devsuperior" target="_blank" rel="noreferrer">
-                <div className="dsmovie-contact-container">
-                    <GithubIcon />
-                    <p className="dsmovie-contact-link">/devsuperior</p>
-                </div>
-            </a>
-        </div>
-    </nav>
-</header>
-  );
+  return <Navbar />;
 }
 
 export default App;
