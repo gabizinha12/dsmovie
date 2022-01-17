b72bd9688e545349aad6ae34dd60dc1e90a815c1
Gabrielle Lima <gabrielle.clima23@gmail.com>
2022-01-16 23:04:28 -0300

    Paginatiom

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -27,18 +27,13 @@ function Listing() {
         setPage(data);
       });
   }, [pageNumber]);
-  const movie = {
-    id: 1,
-    image:
-      "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-    title: "The Witcher",
-    count: 2,
-    score: 4.5,
-  };
 
+  const handlePageChange = (newNumber: number) => {
+    setPageNumber(newNumber);
+  };
   return (
     <>
-      <Pagination />
+      <Pagination page={page} onChange={handlePageChange} />
       <div className="container">
         <div className="row">
           {page.content.map((movie) => {
